1c291bb0f093c87e1581a34f99cf7dfe2f840bb7
RD320.FIT.UET <rd320.fit@gmail.com>
2022-05-04 00:39:02 +0700

    new change

--- source.cpp
+++ source.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int addTwoNumbers(int num1, int num2){
+int addTwoNumbers(){
     return 0;
 }
 
 int checkAndAdd(int a, int b){
-    int result = addTwoNumbers(a, b);
+    int result = addTwoNumbers();
     if(result < 0){
         return -1;
     } else {
